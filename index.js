0beb0bd1d0ec9c0596f3b99f29ad01793339d827
WizardYac5320 <89355030+WizardYac5320@users.noreply.github.com>
2022-12-22 21:49:27 +0000

    Download tests

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 
-
-
 function Test() {
     console.log('Button was clicked');
 
     let input = document.querySelector('input').value;
     console.log(input)
+    
+    <a href="/sfx/2Pac.mp3" download> <a/>
 }
